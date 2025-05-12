a049f61221e70aeca3605ed114ae97139ab3dacb
Ayush Dumasia <ayushdumasia00@gmail.com>
2025-05-12 17:07:55 +0530

    feat ✨: Kafka with Zookeeper (bitnami)

--- kafka/kafka_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
-kubectl create namespace kafka
-helm install kafka bitnami/kafka --namespace kafka
-kubectl get pods -n kafka
-kubectl get svc -n kafka
